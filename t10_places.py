ae1c8583ba1d2233e62586b80bdd8943f5da54cd
guthriet <guthriet@berea.edu>
2018-10-26 11:30:19 -0400

    I got rid of the TODOs because they were yelling at me. Thanks.

--- t10_places.py
+++ t10_places.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Emily Lovell & Scott Heggen      TODO: Change this to your names
-# Username: lovelle & heggens             TODO: Change this to your usernames
+# Author: Thomas Guthrie && Thomas West
+# Username: guthriet && westth
 #
 # Assignment: T10: Oh, the Places You'll Go!
 #
@@ -89,12 +89,6 @@ def extract_place(file_content):
     :return: a tuple representing a single place.
     """
 
-    # TODO   Read the next five lines of the file, we've done the first one for you (name = ...).
-    # TODO   The order of the lines are: name, location, latitude, longitude, and user color.
-    # TODO   Take a look at places.txt to see the structure of the data.
-    # TODO   Just like above (line 37), you need to remove the last character (\n).
-    # TODO   Once you've got the code working for all five lines, add a conditional that checks to see if
-    # TODO   the line starts with a #; if it does, ignore that line.
     name = "pending"
     location = "pending"
     latitude = "pending"
@@ -130,7 +124,6 @@ def extract_place(file_content):
             color = "pending"
     print(color)
 
-    # FIXME Construct a tuple with all five values in the correct order. Don't forget types, and tuples are immutable!
     # Example: place_tuple = ("Scott's example", "Somewhere special", 41, -10, "black")
     place_tuple = (name, location, latitude, longitude, color)      # Finish assembling the tuple!
     return place_tuple
